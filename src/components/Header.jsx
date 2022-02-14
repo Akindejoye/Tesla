a823346b61f37b49a7f6c34ec4293b988fbc8e12
Akindejoye <tydruhill@gmail.com>
2022-02-14 18:25:48 +0100

    State Control of the Navigation Menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { useState } from 'react';
 
 const Container = styled.div`
     min-height: 6rem;
@@ -7,6 +9,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
+    z-index: 44;
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -42,8 +45,48 @@ const RightMenu = styled.div`
 const CustomMenu = styled(MenuIcon)`
     cursor: pointer;
 `;
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background-color: #fff;
+    width: 30rem;
+    z-index: 999;
+    list-style: none;
+    padding-top: 2rem;
+    padding-left: 4rem;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    min-height: 100vh;
+    overflow-y: auto;
+    transform: ${props => props.show ? 'translateY(0)': 'translateY(-100%)'};
+    transition: transform 0.5s ease-in;
+
+    li {
+        padding: 1.5rem 0;
+    }
+
+    a {
+        font-size: 1.5rem;
+        font-weight: 400;
+    }
+`;
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    padding-bottom: 2rem;
+`
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+    font-size: 3rem !important;
+    `
+
 
 const Header = () => {
+    const [burgerStatus, setBurgerStatus] = useState(false);
+
     return ( 
         <Container>
             <a>
@@ -58,8 +101,27 @@ const Header = () => {
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Account</a>
-                <CustomMenu />
+                <CustomMenu onClick={() => setBurgerStatus(true)} />
             </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={() => setBurgerStatus(false)} />
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-In</a></li>
+                <li><a href="#">Test Drive</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadster</a></li>
+                <li><a href="#">Semi</a></li>
+                <li><a href="#">Charging</a></li>
+                <li><a href="#">Powerwall</a></li>
+                <li><a href="#">Commercial Energy</a></li>
+                <li><a href="#">Utilities</a></li>
+                <li><a href="#">Find Us</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Investor Relations</a></li>
+            </BurgerNav>
         </Container>
      );
 }
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Fade } from 'react-reveal';
 
 const Wrap = styled.div`
     width: 100vw;
@@ -81,27 +82,31 @@ const DownArrow = styled.img`
 const Section = ({ title, description, backgroundImg, leftBtnText, rightBtnText, downArrow}) => {
     return ( 
         <Wrap bgImage={backgroundImg}>
-           <ItemText>
-               <Header1>
-                 {title}
-               </Header1>
-               <Header2>
-                 {description}
-               </Header2>
-            </ItemText>
+            <Fade bottom>
+            <ItemText>
+                <Header1>
+                    {title}
+                </Header1>
+                <Header2>
+                    {description}
+                </Header2>
+                </ItemText>
+            </Fade>
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>
-                        {leftBtnText}
-                    </LeftButton>
-                    {
-                        rightBtnText && (
-                        <RightButton>
-                            {rightBtnText}
-                        </RightButton>
-                        )
-                    }
-                </ButtonGroup>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftBtnText}
+                        </LeftButton>
+                        {
+                            rightBtnText && (
+                            <RightButton>
+                                {rightBtnText}
+                            </RightButton>
+                            )
+                        }
+                    </ButtonGroup>
+                </Fade>
                 <DownArrowCentered>
                     {
                         downArrow && (
